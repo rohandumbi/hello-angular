174327a6c275bd0716bdd3bd518045818d968e81
rohghosh <ScoobyDoo_1986>
2017-07-26 01:07:50 +0530

    service layer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 import { HeroDetailComponent } from './components/hero-detail.component';
 import { HeroListComponent } from './components/hero-list.component';
 import { HeroesComponent } from './components/heroes.component';
 import { DashboardComponent } from './components/dashboard.component'
 import { AppComponent } from './components/app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/hero-list.component.ts
+++ src/app/components/hero-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { HeroService } from '../service/hero.service';
 import { Hero } from '../models/hero';
 
 @Component({
   selector: 'hero-list',
   templateUrl: '../templates/hero-list.component.html',
-  styleUrls: ['../stylesheets/hero-list.component.css']
+  styleUrls: ['../stylesheets/hero-list.component.css'],
+  providers: [HeroService]
 })
 
 export class HeroListComponent {
@@ -12,8 +14,19 @@ export class HeroListComponent {
   @Output() notify: EventEmitter<Hero> = new EventEmitter<Hero>();
   selectedHero: Hero;
 
+  constructor(private heroService: HeroService) { }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.notify.emit(hero);
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 }
